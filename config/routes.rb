599f49b7f8e54ad5de40fb61cb5eb387d2e94243
laurene michon <laurene.michon@gmail.com>
2020-11-25 12:17:17 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/add_partner", to: "users#add_partner"
+  resources :names, only: :index
+  resources :choices, only: [ :create, :index, :update ]
+  resources :filters, only: [ :new, :create, :edit, :update ]
+  resources :matches, only: :index
 end
