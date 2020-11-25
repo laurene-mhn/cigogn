047de2def460f417771b6b0b9f9bc9cbde42b82d
Marion Thomy <marionthomy@gmail.com>
2020-11-25 17:43:40 +0100

    first steps form filters

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,7 +1,7 @@
 class Filter < ApplicationRecord
   belongs_to :user
   GENDER = ["girl", "boy", "unisex"]
-  # ORIGIN = ["french", "english", "hebrew", "latin"]
+  ORIGIN = ["french", "english", "hebrew", "latin"]
   ASTROLOGY = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"]
   LENGTH = ["short 0-5ch", "medium 6-9ch", "long 10ch & more"]
   POPULARITY = ["rare", "common", "popular"]
