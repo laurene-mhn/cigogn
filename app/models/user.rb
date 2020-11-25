48fb4ddfdc9dc7c576749c6c18dbc0fa2bedc714
Gregoire Lemesle <gregoire.lemesle@gmail.com>
2020-11-25 17:17:41 +0100

    dependent destroy on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   belongs_to :partner, class_name: "User", optional: true
-  has_many :choices
+  has_many :choices, dependent: :destroy
   has_many :filters
 end
