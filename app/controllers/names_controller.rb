d64ac343e753ea7b57c497db13cd335402ac8cbd
Gregoire Lemesle <gregoire.lemesle@gmail.com>
2020-12-01 20:27:02 +0100

    Done with filter BUT to be checked with new code

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -7,25 +7,44 @@ class NamesController < ApplicationController
       # names = names.where(gender: filter.gender)
         names = names.where('gender = ?', filter.gender)
       end
+
       if filter.origin.any?
         names = names.where("origin && ARRAY[?]::varchar[]", filter.origin)
       end
-      # if filter.length.any?
-      #   names = names.select { |name| name.name.length => filter.length }
-      # end
+      # Fonctionne UNIQUEMENT sur un des filtres
+      if filter.length.any? || filter.length != ["court 0-5car", "long 10car & plus"]
+        case filter.length
+        when ["court 0-5car"]
+          names = names.select{ |name| name.name.length >= 0 && name.name.length <= 5 }
+        when ["moyen 6-9car"]
+          names = names.select{ |name| name.name.length >= 6 && name.name.length <= 9 }
+        when ["long 10car & plus"]
+          names = names.select{ |name| name.name.length >= 10 }
+        when ["court 0-5car", "moyen 6-9car"]
+          names = names.select{ |name| name.name.length >= 0 && name.name.length <= 9 }
+        when ["moyen 6-9car", "long 10car & plus"]
+          names = names.select{ |name| name.name.length >= 6}
+        when ["court 0-5car", "long 10car & plus"]
+          names = names.select{ |name| name.name.length >= 10 || name.name.length <= 5 }
+        when ["court 0-5car","moyen 6-9car", "long 10car & plus"]
+          names = names.select{ |name| name.name.length >= 0}
+        end
+      end
       if filter.popularity.any?
         names = names.where('popularity IN (?)', filter.popularity)
       end
       if filter.astrology.any?
         names = names.where('astrology IN (?)', filter.astrology)
       end
-      # if filter.start_with.any?
-      #   names = names.select { |name| name.name => filter.length }
-      # end
-      # if filter.end_with.any?
-      #   names = names.select { |name| name.name.length > filter.length }
-      # end
-
+      if filter.start_with.any?
+        names = names.select { |name| name.name.start_with? filter.start_with.capitalize }
+      end
+      if filter.end_with.any?
+        names = names.select { |name| name.name.end_with? filter.end_with.capitalize }
+      end
+      if filter.hyphenated_name == true
+        names = names.where("name like ?", "%-%")
+      end
     end
 
     # names = names.where(origin: "french")
