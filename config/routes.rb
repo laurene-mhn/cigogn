2bf3f059093b70a6a75e025d32a0f59a2d0326c2
laurene michon <laurene.michon@gmail.com>
2020-12-02 13:01:51 +0100

    add homapage with logo

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/profile", to: "pages#profile"
   get "/add_partner", to: "users#add_partner"
-  get "/end_list", to: "choices#end_list"
+  get "/end_list", to: "pages#end_list"
   resources :names, only: :index
   resources :choices, only: [ :create, :index, :update ]
   resources :filters, only: [ :new, :create, :edit, :update ]
