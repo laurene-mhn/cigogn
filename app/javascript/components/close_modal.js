69d4baa39b8d3ad2a525a785dc731d76f47f4c2e
Antony Marques <antonymarques97@gmail.com>
2020-12-03 10:08:19 +0100

    Swipe function adjusted + js bugs fixed

--- app/javascript/components/close_modal.js
+++ app/javascript/components/close_modal.js
@@ -1,17 +1,21 @@
 const closeModal = () => {
   const button = document.querySelector(".close");
-  button.addEventListener("click", () => {
-    const modal = document.querySelector(".modal");
-    modal.classList.remove("show");
-  }) 
+  if (button) {
+    button.addEventListener("click", () => {
+      const modal = document.querySelector(".modal");
+      modal.classList.remove("show");
+    }) 
+  }
 }
 
 const closeModalTwo = () => {
   const buttonTwo = document.querySelector(".btn-research");
-  buttonTwo.addEventListener("click", () => {
-    const modal = document.querySelector(".modal");
-    modal.classList.remove("show");
-  }) 
+  if (buttonTwo) {
+    buttonTwo.addEventListener("click", () => {
+      const modal = document.querySelector(".modal");
+      modal.classList.remove("show");
+    }) 
+  }
 }
 
 export {closeModal};
